f7e7bc0f616bf706fcef6b97dae608b69a40a92e
Erik Mingo <mingo@nsidc-seaice.ad.int.nsidc.org>
2014-05-27 12:40:44 -0600

    playing with getting the user's ip address

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask
+from flask import jsonify
+from flask import request
+
 
 app = Flask(__name__)
 
+# Bit to show ip address
+
+@app.route('/get_my_ip', methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip' : request.remote_addr}), 200
 
 @app.route('/')
-
 def hello():
     return "Hello World!"
