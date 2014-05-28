26da192c7424ecb6b46dd43c85d391e60eb3f9e5
Erik Mingo <mingo@nsidc-seaice.ad.int.nsidc.org>
2014-05-28 09:59:20 -0600

    new service

--- hello.py
+++ hello.py
@@ -17,5 +17,5 @@ def get_my_ip():
 
 @app.route('/')
 def hello():
-    return requests.get('http://whatismyip.org').read.encode("utf8") + "julia is a huge boob", 200
+    return requests.get('http://wtfismyip.com/json').read.encode("utf8") + "julia is a huge boob", 200
 
